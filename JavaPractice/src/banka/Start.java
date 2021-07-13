be96a6c1d5c458846cfb0d1e7449a3f73d735ab0
Zoran Đukić <zorandjukic.os@gmail.com>
2021-07-13 14:23:03 +0200

    Banka - drugi dio

--- JavaPractice/src/banka/Start.java
+++ JavaPractice/src/banka/Start.java
@@ -15,20 +15,58 @@ public class Start {
 
 	private void glavniIzbornik() {
 		System.out.println("****Erste Bank****");
-		System.out.println("Molimo Vas unesite PIN");
-		System.out.println("Odustani");
 		provjeraPina();
 	}
 
 	private void provjeraPina() {
 		switch (Ulaz.ucitajPin("Molimo unesite pin", "Pogrešan PIN. Probajte ponovo", 1234)) {
-		case 1:
-			stanjeRacuna();
+		case 1234:
+			drugiIzbornik();
 			break;
 		case 2:
 			System.out.println("Hvala Vam. Doviđenja!");
-			
+			return;
+
 		}
+	}
+
+	private void drugiIzbornik() {
+		System.out.println(" 1. Stanje Vašeg računa");
+		System.out.println(" 2. Isplate s računa");
+		System.out.println(" 3. Uplate na račun");
+		System.out.println(" 4. Kraj rada");
+		ucitajAkciju();
+	}
+
+	private void ucitajAkciju() {
+		switch (Ulaz.ucitajInt("Odaberite akciju: ", "Niste unijeli cijelibroj", 1, 4)) {
+		case 1:
+			stanjeRacuna();
+		case 2:
+			isplataNovca();
+		case 3:
+			uplataNovaca();
+		case 4:
+			System.out.println("Hvala Vam što koristite usluge Erste banke");
+		}
+
+	}
+
+	private void uplataNovaca() {
+
+	}
+
+	private void isplataNovca() {
+
+	}
+
+	private void stanjeRacuna() {
+		stanjeKuna(1250.37);
+	}
+
+	private static double stanjeKuna(double kune) {
+		return kune = 0;
+
 	}
 
 	public static void main(String[] args) {
--- JavaPractice/src/banka/Ulaz.java
+++ JavaPractice/src/banka/Ulaz.java
@@ -5,22 +5,72 @@ import javax.swing.JOptionPane;
 public class Ulaz {
 	public static int ucitajPin(String poruka, String greska, int brojPina) {
 		int broj = 0;
-		while (true) {
+
+		for (int i = 0; i < 4; i++) {
+			if (i == 3) {
+				System.out.println("Tri puta pogrešno upisat PIN. Kartica blokirana.");
+				break;
+
+			}
 			System.out.println(poruka);
 			try {
 
 				broj = Integer.parseInt(JOptionPane.showInputDialog(poruka));
 				if (!(broj == brojPina)) {
-					System.out.println("Pogrešan PIN. Molimo Vas unesite pravi PIN." );
+
 					continue;
 				}
 				break;
 			} catch (Exception e) {
-				// e.printStackTrace();
+				System.out.println(greska);
+
+			}
+
+		}
+		return broj;
+
+	}
+
+	public static int ucitajInt(String poruka, String greska, int min, int max) {
+		int broj = 0;
+
+		while (true) {
+			try {
+				broj = Integer.parseInt(JOptionPane.showInputDialog(poruka));
+
+				if (broj < min || broj > max) {
+					System.out.println("Broj ne smije biti manji od " + min + " ili veći od " + max);
+					continue;
+				}
+				break;
+			} catch (Exception e) {
 				System.out.println(greska);
 			}
+
 		}
 
 		return broj;
 	}
+
+	public static double ucitajDouble(String poruka, String greska, int min, int max) {
+		double broj = 0;
+
+		while (true) {
+			try {
+				broj = Double.parseDouble(JOptionPane.showInputDialog(poruka));
+
+				if (broj < min || broj > max) {
+					System.out.println("Broj ne smije biti manji od " + min + " ili veći od " + max);
+					continue;
+				}
+				break;
+			} catch (Exception e) {
+				System.out.println(greska);
+			}
+
+		}
+
+		return broj;
+	}
+
 }
